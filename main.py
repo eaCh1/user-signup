44ff7c8974d7495ce8e22deb8cafd3bd762317c2
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-26 07:33:22 -0700

    adding condtiionals

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
+username = ""
+password = ""
+email = ""
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("home-page.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+
+
+
+
+    return render_template("home-page.html", username=username, email=email)
+
+@app.route("/welcome", methods=["POST"])
+def welcome():
+    username = request.form['username']
+    return render_template("welcome-page.html", username=username, title="Welcome!")
 
 app.run()
