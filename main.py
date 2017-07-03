2b70b2c71f906fc7e12f7dd3f65cd9d0a947237b
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-07-03 15:15:35 -0700

    done

--- main.py
+++ main.py
@@ -3,27 +3,58 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-username = ""
-password = ""
-email = ""
-
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET'])
 def index():
+    return render_template("home-page.html")
 
-    if request.method == "POST":
-        username = request.form["username"]
-        email = request.form["email"]
+@app.route("/validate", methods=['POST'])
+def validate_info():
+    username = ""
+    password = ""
+    verify = ""
+    email = ""
+    username_error = ""
+    password_error = ""
+    match_error = ""
+
+    if not is_username_valid():
+        username_error = "That's not a valid username"
+    if not is_valid_password():
+        password_error = "That's not a valid password"
+    if not do_passwords_match():
+        match_error = "Passwords don't match"
+
+    if not username_error and not password_error and not match_error:
+        return render_template("welcome-page.html",
+                                username = request.form["username"],
+                                title = "Welcome!")
+    else:
+        return render_template("home-page.html",
+                                username = request.form["username"],
+                                email = request.form["email"],
+                                usernameerror = username_error,
+                                passworderror = password_error,
+                                matcherror = match_error)
 
 
+def is_valid_password():
+    password = request.form["password"]
+    if password != "":
+        return True
+    return False
 
+def is_username_valid():
+    username = request.form["username"]
+    if username != "":
+        return True
+    return False
 
-    return render_template("home-page.html", username=username, email=email)
+def do_passwords_match():
+    password = request.form["password"]
+    verify = request.form["verify"]
+    if password == verify:
+        return True
+    return False
 
-@app.route("/welcome", methods=["POST"])
-def welcome():
-    username = request.form['username']
-    return render_template("welcome-page.html", username=username, title="Welcome!")
 
 app.run()
